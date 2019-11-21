2e6292091082a14f445c6e32820c52038d144998
Cave Uranus <cauranus@mi-o4.21-school.ru>
2019-11-21 05:28:16 +0300

    idk why it doesn't want to delete middle links

--- bfs.c
+++ bfs.c
@@ -6,7 +6,7 @@
 /*   By: cauranus <cauranus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 17:03:30 by cauranus          #+#    #+#             */
-/*   Updated: 2019/11/21 03:19:42 by cauranus         ###   ########.fr       */
+/*   Updated: 2019/11/21 05:25:28 by cauranus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,44 @@ t_links	*find_link(t_links **link, char *name)
 	return (*link);
 }
 
+void	roomdel(t_rooms **room, t_rooms *next)
+{
+		ft_strdel(&(*room)->name);
+		free(*room);
+		*room = next;
+}
+
+void 	linkdel(t_links **links, t_links *next)
+{
+	free(*links);
+	*links = next;
+}
+void	remove_useless_rooms_and_links(t_links **link, t_rooms **room)
+{
+	t_links *links_head;
+	t_rooms *rooms_head;
+	
+
+	links_head = *link;
+	rooms_head = *room;
+	while (*link)
+	{
+		if ((*link)->s->bfs == -1 || (*link)->f->bfs == -1)
+			linkdel(link, (*link)->next);
+		else
+			(*link) = (*link)->next;
+	}
+	while ((*room))
+	{
+		if ((*room)->bfs == -1)
+			roomdel(room, (*room)->next);
+		else
+			(*room) = (*room)->next;
+	}
+	*link = links_head;
+	*room = rooms_head;
+}
+
 void	bfs(t_lem_in *stat)
 {
 	t_queue *queue;
@@ -93,11 +131,16 @@ void	bfs(t_lem_in *stat)
 		find = find_head;
 		bfs++;
 	}
+	remove_useless_rooms_and_links(&stat->links, &stat->rooms);
 	while (stat->rooms)
 	{
 		printf("%s : [%d]\n", stat->rooms->name, stat->rooms->bfs);
-		if (stat->rooms->end && stat->rooms->bfs == -1)
-			printf("end is unreachable\n");
 		stat->rooms = stat->rooms->next;
+	}
+	write(1, "\n", 1);
+	while (stat->links)
+	{
+		printf("s : [%s]\nf : [%s]\n", stat->links->s->name, stat->links->f->name);
+		stat->links = stat->links->next;
 	}
 }
