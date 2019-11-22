7cda2d5d863cc7d2f4a022b0247049228d8b4c0b
Cave Uranus <cauranus@mi-o3.21-school.ru>
2019-11-22 23:27:30 +0300

    fixed incorrect bfs index

--- bfs.c
+++ bfs.c
@@ -6,7 +6,7 @@
 /*   By: cauranus <cauranus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 17:03:30 by cauranus          #+#    #+#             */
-/*   Updated: 2019/11/22 21:07:04 by cauranus         ###   ########.fr       */
+/*   Updated: 2019/11/22 23:26:39 by cauranus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,25 +42,26 @@ int	queueadd(t_queue **queue, t_queue *new)
 	return (1);
 }
 
-void dequeue(t_queue **queue)
+void dequeue(t_queue **queue, t_queue *start)
 {
-	t_queue *buf;
-	
-	if (!*queue)
-		return ;
-	buf = (*queue)->next;
-	free(*queue);
-	*queue = NULL;
-	*queue = buf;
+	(*queue) = (*queue)->next;
+	free(start);
+	start = NULL;
 	
 }
 
-t_links	*find_link(t_links **link, char *name)
+t_links	*find_link(t_links **link, char *name, int bfs)
 {
 	while (*link)
 	{
-		if ((ft_strequ((*link)->s->name, name) && (*link)->f->bfs == -1) || (ft_strequ((*link)->s->name, name) && (*link)->f->bfs == -1))
+		if ((ft_strequ((*link)->s->name, name) && ((*link)->f->bfs == -1 ||(*link)->s->bfs == __INT_MAX__)) || (ft_strequ((*link)->f->name, name) && ((*link)->s->bfs == -1 || (*link)->s->bfs == __INT_MAX__)))
+		{
+			if ((ft_strequ((*link)->f->name, name) && (*link)->s->bfs == -1))
+				((*link)->s->bfs = bfs);
+			else if ((*link)->f->bfs == -1)
+				(*link)->f->bfs = bfs;
 			return (*link);
+		}
 		(*link) = (*link)->next;
 	}
 	return (*link);
@@ -139,6 +140,8 @@ t_links *remove_links(t_links **links)
 		{
 			if ((*links)->s->bfs > (*links)->f->bfs)
 				ft_swap((void**)&(*links)->s, (void**)&(*links)->f);
+			if ((*links)->next->s->bfs > (*links)->next->f->bfs)
+				ft_swap((void**)&(*links)->next->s, (void**)&(*links)->next->f);
 			*links = (*links)->next;
 		}
 	}
@@ -172,20 +175,18 @@ void	bfs(t_lem_in *stat)
 	stat->end->bfs = __INT_MAX__;
 	find_head = stat->links;
 	find = stat->links;
-	bfs = 1;
 	while (queue && queue->room != stat->end)
 	{
-		dequeue(&queue);
-		while ((find = find_link(&find, stat->start->name)))
+		bfs = queue->room->bfs + 1;
+		dequeue(&queue, queue);
+		while ((find = find_link(&find, stat->start->name, bfs)))
 		{
 			queueadd(&queue, init_queue(ft_strequ(stat->start->name, find->f->name) ? find->s :  find->f));
-			queue->room->bfs = bfs;
 			find = find->next;
 		}
 		if (queue)
 			stat->start = queue->room;
 		find = find_head;
-		bfs++;
 	}
 	
 	remove_useless_rooms_and_links(stat);
