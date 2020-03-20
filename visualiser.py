b735bf99c54714fce48aa681152e27a929a81eca
ishcherbina <ishcherbina@phoenixit.ru>
2020-03-20 16:22:07 +0300

    added different colour in path

--- visualiser.py
+++ visualiser.py
@@ -35,20 +35,23 @@ class Link:
 
 
 class Room:
-	def __init__(self, name, x, y):
+	def __init__(self, name, x, y, ispath):
 		self.name = name
 		self.x = x
 		self.y = y
+		self.ispath = ispath
 
 
-def draw(canvas, x1, y1, x2, y2, name1, name2):
-	canvas.create_line(x1, y1, x2, y2, width=2)
-	canvas.create_oval(x1 - NODE_SIZE, y1 - NODE_SIZE,
-					x1 + NODE_SIZE, y1 + NODE_SIZE, outline="black", fill="gray")
-	canvas.create_oval(x2 - NODE_SIZE, y2 - NODE_SIZE,
-					x2 + NODE_SIZE, y2 + NODE_SIZE, outline="black", fill="gray")
-	canvas.create_text(x1, y1, text=name1, font="Times 10 bold", fill='black')
-	canvas.create_text(x2, y2, text=name2, font="Times 10 bold", fill='black')
+def draw(canvas, link):
+	canvas.create_line(link.start.x, link.start.y, link.end.x, link.end.y, width=2)
+	canvas.create_oval(link.start.x - NODE_SIZE, link.start.y - NODE_SIZE,
+					link.start.x + NODE_SIZE, link.start.y + NODE_SIZE, outline="black",
+					fill=("gray" if not link.start.ispath else "yellow"))
+	canvas.create_oval(link.end.x - NODE_SIZE, link.end.y - NODE_SIZE,
+					link.end.x + NODE_SIZE, link.end.y + NODE_SIZE, outline="black",
+					fill=("gray" if not link.end.ispath else "yellow"))
+	canvas.create_text(link.start.x, link.start.y, text=link.start.name, font="Times 10 bold", fill='black')
+	canvas.create_text(link.end.x, link.end.y, text=link.end.name, font="Times 10 bold", fill='black')
 
 
 def main():
@@ -66,16 +69,21 @@ def main():
 	links = re.findall('\w+-\w+', data)
 	rooms = re.findall('\w+ \d+ \d+', data)
 	end = re.findall('##end\n\w', data)[0].split('\n')[1]
-	paths = re.findall('L.*?\n', data)
+	paths = re.findall('L\d+-\w+', data)
+	paths_by_turn = re.findall('L.*?\n', data)
 	# print(rooms)
 	# print(links)
 	print(paths)
 	paths_list = []
+	for path in paths:
+		buf = path.split('-')
+		paths_list.append(buf[1])
 	rooms_list = []
 	links_list = []
 	for room in rooms:
 		buf = room.split(' ')
-		rooms_list.append(Room(buf[0], int(buf[1]) * ZOOM, int(buf[2]) * ZOOM))
+		rooms_list.append(Room(buf[0], int(buf[1]) * ZOOM, int(buf[2]) * ZOOM,
+						buf[0] in paths_list or buf[0] == start or buf[0] == end))
 	for room in rooms_list:
 		print(room.name + " " + str(room.x) + " " + str(room.y))
 	for link in links:
@@ -91,7 +99,7 @@ def main():
 	for path in paths:
 		buf = path.replace('\n', '')
 	for link in links_list:
-		draw(canvas, link.start.x, link.start.y, link.end.x, link.end.y, link.start.name, link.end.name)
+		draw(canvas, link)
 	window.mainloop()
 
 
